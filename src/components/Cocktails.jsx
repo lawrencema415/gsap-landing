6329ccaa51d0fabaad23a5a8dcb46f9f1b7d9ec9
Lawrence Ma <lawrencema415@gmail.com>
2025-06-23 02:13:28 -0700

    Add leaf animation

---
+++ src/components/Cocktails.jsx
@@ -0,0 +1,61 @@
+import {cocktailLists, mockTailLists} from "../constants/index.js";
+import { useGSAP } from "@gsap/react";
+import gsap from 'gsap';
+
+const Cocktails = () => {
+  useGSAP(() => {
+    const parallaxTimeline = gsap.timeline({
+      scrollTrigger: {
+        trigger: '#cocktails',
+        start: 'top 30%',
+        end: 'bottom 80%',
+        scrub: true,
+      }
+    })
+    
+    parallaxTimeline.from('#c-left-leaf', {
+      x: -100,
+      y: 100,
+    }).from('#c-right-leaf', {
+      x: 100, y: 100
+    })
+  })
+  
+  return (
+    <div id='cocktails' className='noisy'>
+      <img id='c-left-leaf' src='/images/cocktail-left-leaf.png' alt='left-leaf' />
+      <img id='c-right-leaf' src='/images/cocktail-right-leaf.png' alt='right-leaf' />
+      <div className='list'>
+        <div className='popular'>
+          <h2>Most popular cocktails:</h2>
+          <ul>
+            {
+              cocktailLists.map(({name, country, detail, price}) => <li key={name}><div className='md:me-28'><h3>{name}</h3>
+              <p>{country} | {detail}</p>
+              </div>
+              <span>{price}</span>
+              </li>)
+              
+            }
+          </ul>
+        </div>
+        
+        <div className='loved'>
+          <h2>Most loved mocktails:</h2>
+          <ul>
+            {
+              mockTailLists.map(({name, country, detail, price}) => <li key={name}><div className='md:me-28'><h3>{name}</h3>
+                <p>{country} | {detail}</p>
+              </div>
+                <span>{price}</span>
+              </li>)
+              
+            }
+          </ul>
+        </div>
+        
+      </div>
+    </div>)
+}
+
+export default Cocktails;
